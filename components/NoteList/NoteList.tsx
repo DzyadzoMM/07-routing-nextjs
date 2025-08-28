80424c5ddbcb5fd593a22116861bfd4b84b6ed87
DzyadzoMM <m.dziadzo@gmail.com>
2025-08-28 18:21:30 +0300

    first

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,40 +1,44 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteNote } from '../../services/noteService';
-import  {Note}  from '../../types/note';
-import css from './NoteList.module.css';
+import css from "./NoteList.module.css";
+import type { Note } from "../../types/note";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "@/lib/api";
+import Link from "next/link";
 
 interface NoteListProps {
-  notes: Note[];
+    notes: Note[];
 }
+
 
 export default function NoteList({ notes }: NoteListProps) {
   const queryClient = useQueryClient();
 
-  const mutation = useMutation({
-    mutationFn: deleteNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['notes'] });
-    },
+const mutation = useMutation({
+  mutationFn: deleteNote,
+  onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: ["notes"] });
+    }
   });
 
-  const handleDelete = (id: string) => {
-    mutation.mutate(id);
-  };
 
-  return (
-    <ul className={css.list}>
-      {notes.map((note) => (
+    return (
+        <ul className={css.list}>
+        {notes.map( note =>
         <li key={note.id} className={css.listItem}>
           <h2 className={css.title}>{note.title}</h2>
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
-            <span className={css.tag}>{note.tag}</span>
-            <button onClick={() => handleDelete(note.id)} className={css.button}>
-              Delete
+              <span className={css.tag}>{note.tag}</span>
+              <Link href={`/notes/${note.id}`} >View details</Link>
+            <button 
+              className={css.button}
+              onClick={() => mutation.mutate(note.id)}
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending ? "Deleting..." : "Delete"}
             </button>
           </div>
         </li>
-      ))}
-    </ul>
-  );
+)}
+</ul>
+    )
 }
